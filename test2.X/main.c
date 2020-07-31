e15e2c575857cca8eb11546f1bfc1d0d1740b649
SunXiaolong <sunxiaolong0620@126.com>
2020-07-31 19:40:53 +0800

    “更改程序保证校验码高压低压一致”

--- test2.X/main.c
+++ test2.X/main.c
@@ -27,7 +27,7 @@ __CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_OFF & MCLRE_ON & CP_ON & CPD_ON & BOREN_
 __CONFIG(WRT_OFF&PLLEN_ON&LVP_OFF&STVREN_OFF&BORV_19&LVP_ON) ;
 #endif*/
 //更改MCU为低压版本 
-#define VERSION_STR     "V1.1"
+#define VERSION_STR     "V1.0"
 /*
  * 先初始化时钟，然后初始化外设（定时器，IO，ADC），PIC子函数调用不能超过8级
  */
@@ -91,7 +91,7 @@ int main(int argc, char** argv)
     //    MOTOR_INB = 1;
         //Delay_MS(500);
         //根据实际测试 缩短上电延时时间控制在9S左右
-        Delay_MS(1200);//增加上电延时时间sxl
+        Delay_MS(1250);//增加上电延时时间sxl
     }
 #endif 
     if(Ctr_Signal == Ctrl_SignalValid)//一上电控制信号就有效
